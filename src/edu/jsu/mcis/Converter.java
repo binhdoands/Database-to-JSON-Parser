fc7d4b7d6a5905999e8127ff5a0d0b654e3d38ea
binhdoands <doanbinhds@gmail.com>
2021-02-21 20:34:29 -0600

    project#2A

--- src/edu/jsu/mcis/Converter.java
+++ src/edu/jsu/mcis/Converter.java
@@ -1,10 +1,14 @@
+
 package edu.jsu.mcis;
 
 import java.io.*;
 import java.util.*;
 import com.opencsv.*;
+import com.opencsv.exceptions.CsvException;
+import net.sf.json.JSONArray;
 import org.json.simple.*;
 import org.json.simple.parser.*;
+
 
 public class Converter {
     
@@ -61,18 +65,99 @@ public class Converter {
         
         String results = "";
         
+        
         try {
             
-            CSVReader reader = new CSVReader(new StringReader(csvString));
-            List<String[]> full = reader.readAll();
-            Iterator<String[]> iterator = full.iterator();
+            /* Create Stringbuilder for JSON*/
+            StringBuilder json= new StringBuilder();
             
-            // INSERT YOUR CODE HERE
+            //BufferedReader reader = new BufferedReader(new FileReader("grades.csv"));
+            CSVReader reader = new CSVReader(new FileReader("grades.csv"));
+            List<String[]> csv= reader.readAll();
+            Iterator<String[]> iterator = csv.iterator();
             
-        }        
-        catch(Exception e) { return e.toString(); }
+            
+            
+            
+            JSONArray rHeaders= new JSONArray();
+            JSONArray cHeaders= new JSONArray();
+            JSONArray data = new JSONArray();
+            String[] rows;
+            
+             /* Get first String[] array from CSV data (the column headers); add elements to "cHeaders" */
+            
+            cHeaders.addAll(Arrays.asList(iterator.next()));
+
+            /* Iterate through remaining CSV rows */
+
+            while (iterator.hasNext()){
+                
+                /* Create container for next row */
+                
+                JSONArray row = new JSONArray();
+                
+                /* Get next String[] array from CSV data */
+                
+                rows = iterator.next();
+                
+                /* Get first element (the row header); add element to "rHeaders" */
+                
+                rHeaders.add(rows[0]);
+                
+                /* Add remaining elements to "data" */
+                
+                for (int i = 1; i < rows.length; i++){
+                    row.add(rows[i]);
+                }
+                
+                /* Add row to "data" */
+                
+                data.add(row);
+            
+            }
+            /* Construct JSON string (remember, this must be an *exact* match for the example!) */
+            
+            /* Add column and row headers */
+
+            json.append("{\n    \"colHeaders\":").append(cHeaders.toString());
+            json.append(",\n    \"rowHeaders\":").append(rHeaders.toString()).append(",\n");
+            
+            /* Split "data" rows */
+            
+            rows = data.toString().split("],");
+            
+            /* Add data */
+
+            json.append("    \"data\":");
+
+            for (int i = 0; i < rows.length; ++i){
+                
+                String s = rows[i];         /* Get next data row */
+
+                s = s.replace("\"","");     /* Delete double-quotes */
+                s = s.replace("]]","]");    /* Fix terminating square brackets */
+                
+                json.append(s);             /* Append row */
+                
+                 /* If this is not the last data row, close the row and begin a new one */
+                
+                if ((i % rows.length) != (rows.length - 1))
+                    json.append("],\n            ");
+                
+            }
+            
+            /* Close JSON string */
+            
+            json.append("\n    ]\n}");
+            
+            
+            
+        
+            }
+        catch(CsvException | IOException e) { return e.toString(); }
         
         return results.trim();
+        
         
     }
     
@@ -83,13 +168,84 @@ public class Converter {
         try {
 
             StringWriter writer = new StringWriter();
-            CSVWriter csvWriter = new CSVWriter(writer, ',', '"', '\n');
+            CSVReader reader = new CSVReader(new FileReader("grades.json"));
             
-            // INSERT YOUR CODE HERE
+            
+             /* Create json-simple JSON Parser */
+            
+            JSONParser parser = new JSONParser();
+            
+            /* Parse JSON Data */
+            
+            JSONObject jobject = (JSONObject) parser.parse(jsonString);
+            JSONArray col = (JSONArray) jobject.get("colHeaders");
+            JSONArray row = (JSONArray) jobject.get("rowHeaders");
+            JSONArray data = (JSONArray) jobject.get("data");
+            
+            /* Create String[] arrays for OpenCSV */
+            
+            String[] csvcol = new String[col.size()];
+            String[] csvrow = new String[row.size()];
+            String[] csvdata = new String[data.size()];
+            String[] rowdata;
+            
+            /* Copy column headers */
+
+            for (int i = 0; i < col.size(); i++) {
+                csvcol[i] = col.get(i) + "";
+            }
+            
+            /* Copy row headers and row data */
+
+            for (int i = 0; i < row.size(); i++) {
+                
+                csvrow[i] = row.get(i) + "";
+                csvdata[i] = data.get(i) + "";
+
+            }
+            
+            /* Create OpenCSV Writer */
+            //StringWriter writer = new StringWriter();
+            //CSVWriter csvWriter;
+            CSVWriter csvWriter = new CSVWriter(new FileWriter("grades.json"));
+            /* Write column headers */
+            //csvWriter = new CSVWriter(writer, ',', '"', '\n');
+
+            csvWriter.writeNext(csvcol);
+
+            /* Write row headers and row data */
+
+            for (int i = 0; i < csvdata.length; i++) {
+                
+                /* Strip square brackets from next row */
+                
+                csvdata[i] = csvdata[i].replace("[","");
+                csvdata[i] = csvdata[i].replace("]","");
+                
+                /* Split csvdata[i] into row elements (using comma as delimiter) */
+
+                String[] elements = csvdata[i].split(",");
+                
+                /* Create String[] container for row data (sized at the number of row elements, plus one for row header) */
+                
+                rowdata = new String[elements.length + 1];
+                
+                /* Copy row header into first element of "rowdata" */
+
+                                rowdata[0] = csvrow[i];
+                
+                /* Copy row elements into remaining elements of "rowdata" */
+                System.arraycopy(csvrow, 1, rowdata, 1, csvrow.length - 1);
+                                
+                
+                /* Write new row */
+                
+                //csvWriter.writeNext(rowdata);
+            }
             
         }
         
-        catch(Exception e) { return e.toString(); }
+        catch(IOException | ParseException e) { return e.toString(); }
         
         return results.trim();
         
